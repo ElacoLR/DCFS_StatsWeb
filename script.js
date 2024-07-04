65b1779096d31a3d6e5a1a8e395a01ec6e5b9a94
Rising_Star <alex010204@gmail.com>
2024-07-04 14:24:05 +0900

    first js

---
+++ script.js
@@ -0,0 +1,3 @@
+const searchForm = document.getElementById("search-form")
+const searchButton = document.getElementById("search-btn")
+const searchErrorMsg = document.getElementById("search-error-msg")
